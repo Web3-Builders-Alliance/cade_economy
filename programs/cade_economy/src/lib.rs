142966b36007b6494f9774e72f98c11d19eba0e5
Ahzam Akhtar <ahzam8186@gmail.com>
2024-03-16 03:40:30 +0530

    Init Added

--- programs/cade_economy/src/lib.rs
+++ programs/cade_economy/src/lib.rs
@@ -1,19 +1,29 @@
-mod errors;
-mod contexts;
-mod state;
-
 use anchor_lang::prelude::*;
+
+pub mod state;
+
+pub use state::*;
+
+pub mod contexts;
+
+pub use contexts::*;
+// mod helpers;
+
+pub mod errors;
+
+pub use errors::AmmError;
 
 declare_id!("KXnRjPHroM6rJTeVy51JPkiBuS5vD87VUdB5a17GPDK");
 
 #[program]
-pub mod cade_economy {
+pub mod newamm {
     use super::*;
-
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        Ok(())
+    pub fn initialize(
+        ctx: Context<Initialize>,
+        seed: u64,
+        authority: Option<Pubkey>,
+    ) -> Result<()> {
+        ctx.accounts.init(&ctx.bumps, seed, authority)
     }
-}
 
-#[derive(Accounts)]
-pub struct Initialize {}
+}
